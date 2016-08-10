ee3ea58d911a51668b6ae5c23308b52f8e3f9d56
Connor Lee <Connorlee@u.boisestate.edu>
2016-08-10 12:25:27 -0600

    api data

--- public/league-service.js
+++ public/league-service.js
@@ -10,34 +10,34 @@
 
        
         ls.getLeagueList = function(sucessCallback, failCallback){
-            var list = getMasterLeagueList();
-            if(list){
-                return sucessCallback(list);
-            }
+            // var list = getMasterLeagueList();
+            // if(list){
+            //     return sucessCallback(list);
+            // }
             $http({
                 method: 'GET',
                 url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=RGAPI-689BE4DC-5FF5-4088-B688-808979F36E57'
             }).then(function successCallback(response) {
-                saveMasterLeagueList(response.data.results)
-                console.log(response)
-                return masterLeagueList()
+                // saveMasterLeagueList(response.data.results)
+                console.log(response.data.data)
+                // return masterLeagueList()
             }, function errorCallback(response) {
 
             });
 
         
 
-        function saveMasterLeagueList(data){
-            localStorage.setItem('masterLeagueList', JSON.stringify(data));
-        }
-        function getMasterLeagueList(){
-            var data = localStorage.getItem('masterLeagueList');
-            if(data){
-                masterLeagueList = JSON.parse(list);
-            }
+        // function saveMasterLeagueList(data){
+        //     localStorage.setItem('masterLeagueList', JSON.stringify(data));
+        // }
+        // function getMasterLeagueList(){
+        //     var localData = localStorage.getItem('masterLeagueList');
+        //     if(localData){
+        //         masterLeagueList = JSON.parse(localData);
+        //     }
         
-            return JSON.parse(list);
-        }
+        //     return JSON.parse(data);
+        // }
         }
     }
 }());
