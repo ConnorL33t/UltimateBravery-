f0be59e8e20c18e4aff85c748f6cd795f338a753
Connor Lee <Connorlee@u.boisestate.edu>
2016-08-10 22:47:33 -0600

    Sorted champs by alphabet round 2

--- public/ultimatebraveryapp/components/landingpage.js
+++ public/ultimatebraveryapp/components/landingpage.js
@@ -1,36 +1,40 @@
-;(function(){
+; (function () {
 
 
 
-angular.module('ultimateBravery')
-    .component('lPComponent', {
-        templateUrl: 'ultimatebraveryapp/templates/landingpage.html',
-        controller: LPController
-    
-    })
-        LPController.$inject = ['$state','LeagueService'];
+    angular.module('ultimateBravery')
+        .component('lPComponent', {
+            templateUrl: 'ultimatebraveryapp/templates/landingpage.html',
+            controller: LPController
+
+        })
+    LPController.$inject = ['$state', 'LeagueService'];
 
 
-    function LPController ($state, LeagueService){
-    var $ctrl = this;
+    function LPController($state, LeagueService) {
+        var $ctrl = this;
 
-    $ctrl.toggle = function(){
-        
-    }
-    LeagueService.getLeagueList(function(champs){
-        Object.keys(champs).sort().forEach(function (x, i){
-             console.log(x, champs[x])           
+        $ctrl.toggle = function () {
+
+        }
+        $ctrl.champArray = [];
+        LeagueService.getLeagueList(function (champs) {
+            Object.keys(champs).sort().forEach(function (x, i) {
+                $ctrl.champArray.push(champs[x])
+            })
+            // $ctrl.champArray is an array sorted alphabetically by index, console logging will not work outside of 
+            // LeagueService.getLeagueList because of async (gotta love async)
+            // also figured out that you can only get 99 champs at once so this function is being executed twice. 
+                console.log($ctrl.champArray[0])
+                console.log($ctrl.champArray[131])
+       
         })
 
-        $ctrl.champs = champions
-       
-    
-    })
-
-    
-    
-
-}
 
 
-}());
+
+
+    }
+
+
+} ());
