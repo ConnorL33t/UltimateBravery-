3980129260afd520c0fe61b097260c29eb190b33
Dakota Miller <mill4605@vandals.uidaho.edu>
2016-08-10 20:00:01 -0600

    made images into buttons but buttons DO NOT WORK or turn grey...yet..

--- public/ultimatebraveryapp/components/landingpage.js
+++ public/ultimatebraveryapp/components/landingpage.js
@@ -13,16 +13,14 @@ angular.module('ultimateBravery')
 
     function LPController ($state, LeagueService){
     var $ctrl = this;
+
+    $ctrl.toggle = function(){
+        
+    }
     LeagueService.getLeagueList(function(champs){
         $ctrl.champs = champs
        
-    // var sortChamps = [];
-    //     for(var champ in champs){
-    //         champ.push(sortChamps)
-    //         sortChamps.sort(function(a,b){
-    //             return a[1] - b[1]
-    //         })
-        
+    
     })
 
     
