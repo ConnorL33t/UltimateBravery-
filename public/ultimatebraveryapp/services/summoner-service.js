7907c1b444a8dc725dbfc4b5c0ebf902a170ef63
Nathan Elg <elgn@students.wwu.edu>
2016-08-11 19:14:52 -0600

    name input changes to clean name from api

--- public/ultimatebraveryapp/services/summoner-service.js
+++ public/ultimatebraveryapp/services/summoner-service.js
@@ -15,36 +15,20 @@
         //     localStorage.setItem('champs', JSON.stringify(champs))
         // }
        
-        ss.getSummonerProfile = function(cb){
-        
-            
-        
+        ss.getSummonerProfile = function(summoner, cb){
+            if(!summoner){return}
 
             var base = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/',
-            summoner = '{{$ctrl.user.summoner}}',
-            apikey = 'api_key=RGAPI-689BE4DC-5FF5-4088-B688-808979F36E57'
+            apikey = '?api_key=RGAPI-689BE4DC-5FF5-4088-B688-808979F36E57'
             var url = base+summoner+apikey
 
 
-             $http({
-                method: 'GET',
-                url: url
-            
-            }).then(function successCallback(response) {
-        
-        
-
-        
+             $http.get(url).then(function(response) {
                 var summonerObject = response.data;
-                console.log(summonerObject)
-
-                
-               
-                
-           
+                return cb(summonerObject)
                 // return masterLeagueList()
-            }, function errorCallback(response) {
-
+            }, function(response) {
+                debugger
             });
 
         
