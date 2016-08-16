21ab3a8593f52ad4850289e52c90ad6957c5f712
Connor Lee <Connorlee@u.boisestate.edu>
2016-08-15 22:22:06 -0600

    broke some stuff but i'll fix

--- public/ultimatebraveryapp/services/summoner-service.js
+++ public/ultimatebraveryapp/services/summoner-service.js
@@ -17,36 +17,28 @@
         ss.summoner = ''
         ss.getSummonerProfile = function (summoner, cb) {
             if (!summoner) { return }
-            ss.summoner = summoner
             $http.get('/summoner/' + summoner).then(function (response) {
                 var summonerObject = response.data;
-                console.log(summonerObject)
-                ss.getSummChamps(ss.summoner, summonerObject)
                 return cb(summonerObject)
-                // return masterLeagueList()
-            }, function (response) {
+                
+            
+            
+            })
+            };
+        
+        ss.getSummonersChamps = function(summoner, cb){
+            var summoner = summoner
+            if(!summoner){return}
+            $http.get('/summoner/'+ summoner).then(function(response){
+                // console.log(response.data)
+            return console.log(response.data[summoner].id), $http.get('/summonerstat/'+ response.data[summoner].id).then(function(response){
 
-            });
-            // for(var summoner in summonerObject){
-            //     debugger
-            //     summoner[summonerObject].profileicon = `/image/lolimages/img/profileicon/${summoner}.png`
-            // }
+              var champMastery = response.data
+              return cb(champMastery)
 
-            ss.getSummChamps = function (summoner, summonerObject) {
-                var id = summonerObject[summoner].id
-                var baseUrl = 'https://na.api.pvp.net/championmastery/location/NA1/player/'
-                var endUrl = '/champions?api_key=RGAPI-689BE4DC-5FF5-4088-B688-808979F36E57'
-                var url = baseUrl + id + endUrl
-                $http.get(url).then(function(response){
-                    var summonerchamps = response.data
-                    console.log(response.data)
-                })
-
-            }
-
+            })
+            })
         }
-    }
+            }
+    
 } ());
-
-
-// '{{$ctrl.user.summoner}}'
