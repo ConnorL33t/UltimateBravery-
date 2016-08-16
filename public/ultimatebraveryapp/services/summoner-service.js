1c84938f087091783e90b91af2aee763003a7891
Dakota Miller <mill4605@vandals.uidaho.edu>
2016-08-16 15:06:19 -0600

    SHITS FIXED PULL

--- public/ultimatebraveryapp/services/summoner-service.js
+++ public/ultimatebraveryapp/services/summoner-service.js
@@ -1,109 +1,88 @@
-; (function () {
-    angular.module('ultimateBravery')
-        .service('SummonerService', SummonerService)
+ (function () {
+   angular.module('ultimateBravery')
+       .service('SummonerService', SummonerService)
+   
 
-    SummonerService.$inject = ['$http'];
+   SummonerService.$inject = ['$http', 'LeagueService'];
 
-    function SummonerService($http) {
-
-
-
-
-        var ss = this;
-
-        ss.getRandChampion = function (champ) {
-
-            console.log(champ)
-            console.log(Object.keys(champ.champs))
-
-
-            // GETS random champ number from the list of owned champs for that user
-
-            let randomChampNumber = Math.floor(Math.random() * Object.keys(champ).length)
-
-            console.log(randomChampNumber)
-
-
-            // Finds that champ by matching randomChampNumber to our object of owned champs
-
-
-            var champName = Object.keys(champ.champs).find(function (champ, i) {
-
-
-            // Finds that champ by matching randomChampNumber to our object of owned champs
-
-            var champName = Object.keys(champ.champs).find(function (champ, i) {
-
-
-                if (i == randomChampNumber) {
-                    debugger
-                    return champ;
-                }
-            })
-
-            console.log(champName)
-
-
-            // TODO :: make this next function get the object not a string.
-
-            // var randChamp = [champName];
-
-            // console.log('YOU HAVE BEEN ASSIGNED', randChamp)
+   function SummonerService($http, LeagueService) {
+   
 
 
 
+       var ss = this;
+
+       ss.getRandChampion = function(champ, LeagueService){
+
+           console.log(champ)
+           console.log(Object.keys(champ.champs))
 
 
-        
-        // var SummonerInfo = [];
+           // GETS random champ number from the list of owned champs for that user
+       
+            let randomChampNumber = Math.floor(Math.random() * Object.keys(champ).length)
 
-        // function saveM(champs){
-        //     localStorage.setItem('champs', JSON.stringify(champs))
-        // }
-        ss.summoner = ''
-        ss.getSummonerProfile = function (summoner, cb) {
-            if (!summoner) { return }
-            $http.get('/summoner/' + summoner).then(function (response) {
-                var summonerObject = response.data;
-                return cb(summonerObject)
+            console.log(randomChampNumber)
 
 
+            // Finds that champ by matching randomChampNumber to our object of owned champs
 
+           var champName = Object.keys(champ.champs).find(function(champ, i){
+           
+               if(i == randomChampNumber){
+                   debugger
+                   return champ;
+               }
+           })
 
-            })
-        };
+           console.log(champName)
+           debugger
+           
 
-        ss.getSummonersChamps = function (summoner, cb) {
-            var summoner = summoner
-            if (!summoner) { return }
-<<<<<<< HEAD
-            $http.get('/summoner/' + summoner).then(function (response) {
-                // console.log(response.data)
-                return console.log(response.data[summoner].id), $http.get('/summonerstat/' + response.data[summoner].id).then(function (response) {
+           // TODO :: make this next function get the object not a string.
 
-                    var champMastery = response.data
-                    return cb(champMastery)
+           var randChamp = localStorage.getItem(champs[champName], JSON.parse(champs[champName]));
+           console.log(randChamp)
+           
 
-=======
-            $http.get('/summoner/' + summoner).then(function (result) {
-                $http.get('/summonerstat/' + result.data[summoner].id).then(function (response) {
->>>>>>> 8d9509f17a4d9127fb0262ca1389446b7e42638d
-                })
-            })
-        }
+           console.log('YOU HAVE BEEN ASSIGNED', randChamp)
+           
+           
 
-<<<<<<< HEAD
-    
+           
 
+       }
+       // var SummonerInfo = [];
 
+       // function saveM(champs){
+       //     localStorage.setItem('champs', JSON.stringify(champs))
+       // }
+       ss.summoner = ''
+       ss.getSummonerProfile = function (summoner, cb) {
+           if (!summoner) { return }
+           $http.get('/summoner/' + summoner).then(function (response) {
+               var summonerObject = response.data;
+               return cb(summonerObject)
+               
+           
+           
+           })
+           };
+       
+       ss.getSummonersChamps = function(summoner, cb){
+           var summoner = summoner
+           if(!summoner){return}
+           $http.get('/summoner/'+ summoner).then(function(response){
+               // console.log(response.data)
+           return console.log(response.data[summoner].id), $http.get('/summonerstat/'+ response.data[summoner].id).then(function(response){
 
+             var champMastery = response.data
+             return cb(champMastery)
 
+           })
+           })
+       }
+
+           }
+   
 } ());
-
-
-
-
-=======
-    }
-} ());
->>>>>>> 8d9509f17a4d9127fb0262ca1389446b7e42638d
