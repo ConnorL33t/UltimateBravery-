b01e0cca6640553d6e21c674c2279977d53c970e
Dakota Miller <mill4605@vandals.uidaho.edu>
2016-08-15 21:33:27 -0600

    Got items console logging into variables!

--- public/ultimatebraveryapp/services/league-service.js
+++ public/ultimatebraveryapp/services/league-service.js
@@ -7,15 +7,16 @@
     function LeagueService($http){
         var ls = this;
         var MasterLeagueList = [];
-        var MasterSpellList = [];
+        var MasterItemList = [];
 
         function saveMasterLeagueList(champs){
             localStorage.setItem('champs', JSON.stringify(champs))
         }
 
-         function saveMasterSpellList(champSpells){
-            localStorage.setItem('champSpells', JSON.stringify(champSpells))
+        function saveMasterItemList(items){
+            localStorage.setItem('items', JSON.stringify(items))
         }
+        
        
         this.getLeagueList = function(cb){
 
@@ -46,17 +47,36 @@
                
                 saveMasterLeagueList(champs)
                 cb(champs)
+                console.log(champs)
            
+                $http({
+                method: 'GET',
+                url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?api_key=RGAPI-689BE4DC-5FF5-4088-B688-808979F36E57'
+            }).then(function successCallback(response) {
+                var items = response.data.data;
+                
+                for(var item in items){
+                     items[item].img = `/image/lolimages/img/item/${item}.png`
+                }
+            
+               
+                saveMasterItemList(items)
+                cb(items)
+                console.log(items)                
+
                 // return masterLeagueList()
             }, function errorCallback(response) {
 
             });
+            }, function errorCallback(response) {
 
-
-        
-
-      
+            });
         }
+
+       
+        
+       
+      
 
 
         
