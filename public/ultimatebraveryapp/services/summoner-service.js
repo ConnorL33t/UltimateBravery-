6b8f559e9a55695082c48dcf1a5b1615a0dd3eab
Connor Lee <Connorlee@u.boisestate.edu>
2016-08-16 13:52:47 -0600

    sinkin up man

--- public/ultimatebraveryapp/services/summoner-service.js
+++ public/ultimatebraveryapp/services/summoner-service.js
@@ -5,47 +5,47 @@
     SummonerService.$inject = ['$http'];
 
     function SummonerService($http) {
-    
+
 
 
 
         var ss = this;
 
-        ss.getRandChampion = function(champ){ 
+        ss.getRandChampion = function (champ) {
 
             console.log(champ)
             console.log(Object.keys(champ.champs))
 
 
             // GETS random champ number from the list of owned champs for that user
-        
-             let randomChampNumber = Math.floor(Math.random() * Object.keys(champ).length)
 
-             console.log(randomChampNumber)
+            let randomChampNumber = Math.floor(Math.random() * Object.keys(champ).length)
+
+            console.log(randomChampNumber)
 
 
-             // Finds that champ by matching randomChampNumber to our object of owned champs
+            // Finds that champ by matching randomChampNumber to our object of owned champs
 
-            var champName = Object.keys(champ.champs).find(function(champ, i){
-            
-                if(i == randomChampNumber){
+            var champName = Object.keys(champ.champs).find(function (champ, i) {
+
+                if (i == randomChampNumber) {
                     debugger
                     return champ;
                 }
             })
 
             console.log(champName)
-            
+
 
             // TODO :: make this next function get the object not a string.
 
             // var randChamp = [champName];
 
             // console.log('YOU HAVE BEEN ASSIGNED', randChamp)
-            
-            
 
-            
+
+
+
 
         }
         // var SummonerInfo = [];
@@ -59,26 +59,21 @@
             $http.get('/summoner/' + summoner).then(function (response) {
                 var summonerObject = response.data;
                 return cb(summonerObject)
-                
-            
-            
+
+
+
             })
-            };
-        
-        ss.getSummonersChamps = function(summoner, cb){
+        };
+
+        ss.getSummonersChamps = function (summoner, cb) {
             var summoner = summoner
-            if(!summoner){return}
-            $http.get('/summoner/'+ summoner).then(function(result){
-    
-            return $http.get('/summonerstat/', {params: {"created_by": result.data[summoner].id}}).then(function(response){
-
-              var champMastery = response.data
-              console.log(champMastery)
-              return cb(champMastery)
-
-            })
+            if (!summoner) { return }
+            $http.get('/summoner/' + summoner).then(function (result) {
+                $http.get('/summonerstat/' + result.data[summoner].id).then(function (response){
+              console.log(response)
+                })
             })
         }
-            }
-    
-} ());
+
+    }
+    } ());
