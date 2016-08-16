4dd69a31cf17e47ab9e9331290d0c2f2890462d0
Connor Lee <Connorlee@u.boisestate.edu>
2016-08-16 14:11:47 -0600

    sync

--- public/ultimatebraveryapp/services/league-service.js
+++ public/ultimatebraveryapp/services/league-service.js
@@ -19,15 +19,11 @@
         
        
         this.getLeagueList = function(cb){
-
             $http({
                 method: 'GET',
                 url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=RGAPI-689BE4DC-5FF5-4088-B688-808979F36E57'
             }).then(function successCallback(response) {
-                var champs = response.data.data;
-                
-
-                
+                var champs = response.data.data;             
                 for(var champ in champs){
                     champs[champ].img = `/image/lolimages/img/champion/${champ}.png`
                     champs[champ].spells = [
