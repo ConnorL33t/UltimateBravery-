001e45a8e2f5679b3f7dffb0dccc45a51fa5a640
Connor Lee <connorlee@u.boisestate.edu>
2016-11-24 17:40:18 -0700

    comment because i always forget

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
+// import dependencies
 const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+// import modules
 const {generateMessage} = require('./utils/message')
+// set up server / sockets / path to front end
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+
+// serve
 app.use(express.static(publicPath));
+
+// socket events
 
 io.on('connection', (socket) => {
     console.log('new user connected');
